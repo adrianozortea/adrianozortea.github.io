0af1db9d805bc3695038204836676348bc5a527f
adrianozortea <adriano-pzo@hotmail.com>
2019-12-15 23:56:01 -0300

    Earth
    Terra adicionada, adicionando textura e produzindo o efeito de órbita.
    
    Co-Authored-By: adrianozortea <adrianozortea@users.noreply.github.com>

--- trabalho.js
+++ trabalho.js
@@ -1,3 +1,5 @@
+
+//itens da estrutura inicial
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -6,7 +8,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 
-// Sun (spotlight).
+// Sol com iluminação no centro
 var sun = new THREE.SphereGeometry(3, 50, 50);
 var sunLight = new THREE.PointLight(0xffffff); 
 sunLight.position.set(0, 0, 0); 
@@ -19,16 +21,37 @@ sunLight.add(new THREE.Mesh(sun, new THREE.MeshBasicMaterial({ color: 0xffa500 }
 sunLight.shadowCameraFov = 30;
 scene.add(sunLight);
 
-
-// Extra lighting.
+//iluminação padrão de todos os componentes
 var light = new THREE.PointLight( 0xffffff, 0.5, 100 );
 light.position.set(0, 0, 50);
 scene.add( light );
 
+//earth
+var geometry = new THREE.SphereGeometry(0.6, 20, 20);
+var material  = new THREE.MeshPhongMaterial();
+material.map    = THREE.ImageUtils.loadTexture('images/earthmap1k.jpg');
+var earth = new THREE.Mesh( geometry, material );
+earth.position.set(20, 0, -20);
+scene.add(earth);
 
 
-
-// Camera options.
+// Camera
 camera.position.z = 35;
 
-renderer.render(scene, camera);
+
+function render() {
+  requestAnimationFrame(render);
+  var time = Date.now() * 0.0005;
+
+  // camera.rotation.z+=0.01;
+
+  //earth.rotation.x += 0.01;
+  earth.position.x = Math.sin( time * 1.5 ) * 13;
+  earth.position.z = Math.cos( time * 1.5 ) * 13;
+  // controls.update();
+  
+  
+  renderer.render(scene, camera);
+}
+render();
+
