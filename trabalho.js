24c3eb05124b92323be7309c79b34f9a62bac494
adrianozortea <adriano-pzo@hotmail.com>
2019-12-16 16:10:18 -0300

    camera rotation

--- trabalho.js
+++ trabalho.js
@@ -43,25 +43,65 @@ earth.position.set(20, 0, -20);
 scene.add(earth);
 
 
+// Criando estrelas por pontos randomicos.
+var particles = new THREE.CircleGeometry(0.1, 20);
+for (var p = 0; p < 1000; p++) {
+    var particle = new THREE.Vector3(Math.random() * 500 - 250, Math.random() * 500 - 250, Math.random() * 1000 - 500);
+    particles.vertices.push(particle);
+}
+
+for (var p = 0; p < 500; p++) {
+    var particle = new THREE.Vector3(Math.random() * 300 - 250, Math.random() * 300 - 250, Math.random() * 500 - 400);
+    particles.vertices.push(particle);
+}
+
+
+var particleMaterial = new THREE.ParticleBasicMaterial({ color: 0xffffff, size: 0.1 });
+var particleSystem = new THREE.ParticleSystem(particles, particleMaterial);
+scene.add(particleSystem);
+
+
+
+
 // Camera
-camera.position.z = 15;
+camera.position.z = 35;
 
 
-
+var angle = 360;
 function setupKeyControls() {
+
   document.onkeydown = function(e) {
     switch (e.keyCode) {
-      case 37:
-      camera.position.x -= 1;
+      case 65:
+      camera.position.x -= 0.5;
+      break;
+      case 68:
+      camera.position.x += 0.5;
+      break;
+      case 83:
+      camera.position.y -= 0.5;
+      break;
+      case 87:
+      camera.position.y += 0.5;
       break;
       case 38:
-      camera.position.z -= 1;
-      break;
-      case 39:
-      camera.position.x += 1;
+      camera.position.z -= 0.5;
       break;
       case 40:
-      camera.position.z += 1;
+      camera.position.z += 0.5;
+      break;
+      case 37:
+      camera.rotation.y+=0.01;
+      break;
+      case 39:
+       camera.rotation.y-=0.01;
+      break;
+      case 39:
+       if( model_view == 1){
+        model_view == 0;
+       }else{
+        model_view = 1;
+       }
       break;
     }
   };
@@ -75,9 +115,10 @@ function render() {
   requestAnimationFrame(render);
   var time = Date.now() * 0.0005;
 
-  // camera.rotation.z+=0.01;
+ 
 
   earth.rotation.y += 0.01;
+  earth.position.y = Math.sin( time )* Math.cos(time) *5;
   earth.position.x = Math.sin( time * 1.5 ) * 13;
   earth.position.z = Math.cos( time * 1.5 ) * 13;
   // controls.update();
