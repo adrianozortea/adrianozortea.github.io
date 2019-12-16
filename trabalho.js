69846a920a95355f2314af3297a93aef6b56b3bc
adrianozortea <adriano-pzo@hotmail.com>
2019-12-16 00:53:22 -0300

    Earth texture, Camera movement
    Co-Authored-By: adrianozortea <adrianozortea@users.noreply.github.com>

--- trabalho.js
+++ trabalho.js
@@ -30,13 +30,45 @@ scene.add( light );
 var geometry = new THREE.SphereGeometry(0.6, 20, 20);
 var material  = new THREE.MeshPhongMaterial();
 material.map    = THREE.ImageUtils.loadTexture('images/earthmap1k.jpg');
+
+material.bumpMap    = THREE.ImageUtils.loadTexture('images/earthbump1k.jpg')
+material.bumpScale = 0.05;
+
+material.specularMap    = THREE.ImageUtils.loadTexture('images/original.jpg')
+material.specular  = new THREE.Color('grey')
+
+
 var earth = new THREE.Mesh( geometry, material );
 earth.position.set(20, 0, -20);
 scene.add(earth);
 
 
 // Camera
-camera.position.z = 35;
+camera.position.z = 15;
+
+
+
+function setupKeyControls() {
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+      case 37:
+      camera.position.x -= 1;
+      break;
+      case 38:
+      camera.position.z -= 1;
+      break;
+      case 39:
+      camera.position.x += 1;
+      break;
+      case 40:
+      camera.position.z += 1;
+      break;
+    }
+  };
+}
+
+
+
 
 
 function render() {
@@ -45,13 +77,22 @@ function render() {
 
   // camera.rotation.z+=0.01;
 
-  //earth.rotation.x += 0.01;
+  earth.rotation.y += 0.01;
   earth.position.x = Math.sin( time * 1.5 ) * 13;
   earth.position.z = Math.cos( time * 1.5 ) * 13;
   // controls.update();
   
-  
+  //camera seguindo terra
+  // camera.position.x = earth.position.x;
+  // camera.position.y = earth.position.y;
+  // camera.position.z = earth.position.z+2;
+
+  setupKeyControls();
   renderer.render(scene, camera);
 }
 render();
+
+
+
+
 
